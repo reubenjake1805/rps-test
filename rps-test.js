8e38496d724acc66e7760700da7cb62ff66c5995
Reuben Jake <66435965+reubenjake1805@users.noreply.github.com>
2025-03-26 23:45:05 +0530

    getHumanChoice function built

--- rps-test.js
+++ rps-test.js
@@ -1,25 +1,34 @@
 // This is the file for the rps project
 
-console.log("Hello World");
+console.log("Ready your hands!");
 
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
+let computerChoice = "dummy";
+let humanChoice = "dummy";
+
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-let computerChoice = "dummy";
 let x = Math.floor(Math.random()*100);
 
 if(x <= 33){
-    let computerChoice = "rock";
+    let computerChoice = rock;
     return computerChoice;
 }
 if(x > 33 && x <= 66){
-    let computerChoice = "paper";
+    let computerChoice = paper;
     return computerChoice;
 }
 if(x > 66 && x <= 99){
-    let computerChoice = "scissors";
+    let computerChoice = scissors;
     return computerChoice;
 }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Make your choice!");
+    return humanChoice.toLowerCase();
 }
