1fff7a637906047cc1306c14de7fc5ffa9b841b4
Reuben Jake <66435965+reubenjake1805@users.noreply.github.com>
2025-03-27 00:01:12 +0530

    playRound function built

--- rps-test.js
+++ rps-test.js
@@ -11,24 +11,75 @@ let humanChoice = "dummy";
 let humanScore = 0;
 let computerScore = 0;
 
+let postRoundMessage = `dummy`;
+let currentScore = `Your score is ${humanScore} and the computer's score is ${computerScore}`;
+
 function getComputerChoice() {
 let x = Math.floor(Math.random()*100);
 
 if(x <= 33){
-    let computerChoice = rock;
+    computerChoice = rock;
     return computerChoice;
 }
 if(x > 33 && x <= 66){
-    let computerChoice = paper;
+    computerChoice = paper;
     return computerChoice;
 }
 if(x > 66 && x <= 99){
-    let computerChoice = scissors;
+    computerChoice = scissors;
     return computerChoice;
 }
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Make your choice!");
+    humanChoice = prompt("Make your choice!");
     return humanChoice.toLowerCase();
+}
+
+function playRound() {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+
+    if(humanChoice == "rock" && computerChoice == "rock") {
+        postRoundMessage = `Both of you chose rock. It's a draw! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        return postRoundMessage;
+    }
+    if(humanChoice == "paper" && computerChoice == "paper") {
+        postRoundMessage = `Both of you chose paper. It's a draw! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        return postRoundMessage;
+    }
+    if(humanChoice == "scissors" && computerChoice == "scissors") {
+        postRoundMessage = `Both of you chose scissors. It's a draw! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        return postRoundMessage;
+    }
+    if(humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        postRoundMessage = `Scissors beats paper. You win! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        return postRoundMessage;
+    }
+    if(humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore++;
+        postRoundMessage = `Rock beats scissors. You lose! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        return postRoundMessage;
+    }
+    if(humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++;
+        postRoundMessage = `Paper beats rock. You win! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        return postRoundMessage;
+    }
+    if(humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore++;
+        postRoundMessage = `Scissors beats paper. You lose! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        return postRoundMessage;
+    }
+    if(humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++;
+        postRoundMessage = `Rock beats scissors. You win! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        return postRoundMessage;
+    }
+    if(humanChoice == "rock" && computerChoice == "paper") {
+        computerScore++;
+        postRoundMessage = `Paper beats rock. You lose! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        return postRoundMessage;
+    }
 }
