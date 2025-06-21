f57c4b323f3029db9e4397cdad4a98812914c8e3
Reuben Jake <66435965+reubenjake1805@users.noreply.github.com>
2025-06-21 20:18:03 +0530

    rps with ui

--- rps-test.js
+++ rps-test.js
@@ -1,6 +1,6 @@
 // This is the file for the rps project
 
-
+window.onload = function () {
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
@@ -13,6 +13,29 @@ let computerScore = 0;
 let postRoundMessage = `dummy`;
 let currentScore = `Your score is ${humanScore} and the computer's score is ${computerScore}`;
 let finalWinner = "blank";
+
+
+const rockbtn = document.getElementById('rock');
+const paperbtn = document.getElementById('paper');
+const scissorsbtn = document.getElementById('scissors');
+
+const resultDisplay = document.getElementById('game-results');
+const textDisplay = document.getElementById('game-text');
+
+rockbtn.addEventListener('click', function(){
+    humanChoice = 'rock';
+    playRound();
+});
+
+paperbtn.addEventListener('click', function(){
+    humanChoice = 'paper';
+    playRound();
+});
+
+scissorsbtn.addEventListener('click', function(){
+    humanChoice = 'scissors';
+    playRound();
+});
 
 function getComputerChoice() {
     let x = Math.floor(Math.random()*100);
@@ -37,51 +60,57 @@ function getHumanChoice() {
 }
 
 function playRound() {
-    
-    humanChoice = getHumanChoice();
+    if (humanScore >= 5 || computerScore >= 5) return;
     computerChoice = getComputerChoice();
 
     if(humanChoice == "rock" && computerChoice == "rock") {
-        postRoundMessage = `Both of you chose rock. It's a draw! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        postRoundMessage = `Both of you chose rock. It's a draw! Your score is ${humanScore} and the computer's score is ${computerScore}.`;
         console.log(postRoundMessage);
     }
     if(humanChoice == "paper" && computerChoice == "paper") {
-        postRoundMessage = `Both of you chose paper. It's a draw! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        postRoundMessage = `Both of you chose paper. It's a draw! Your score is ${humanScore} and the computer's score is ${computerScore}.`;
         console.log(postRoundMessage);
     }
     if(humanChoice == "scissors" && computerChoice == "scissors") {
-        postRoundMessage = `Both of you chose scissors. It's a draw! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        postRoundMessage = `Both of you chose scissors. It's a draw! Your score is ${humanScore} and the computer's score is ${computerScore}.`;
         console.log(postRoundMessage);
     }
     if(humanChoice == "scissors" && computerChoice == "paper") {
         humanScore++;
-        postRoundMessage = `Scissors beats paper. You win! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        postRoundMessage = `Scissors beats paper. You win! Your score is ${humanScore} and the computer's score is ${computerScore}.`;
         console.log(postRoundMessage);
     }
     if(humanChoice == "scissors" && computerChoice == "rock") {
         computerScore++;
-        postRoundMessage = `Rock beats scissors. You lose! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        postRoundMessage = `Rock beats scissors. You lose! Your score is ${humanScore} and the computer's score is ${computerScore}.`;
         console.log(postRoundMessage);
     }
     if(humanChoice == "paper" && computerChoice == "rock") {
         humanScore++;
-        postRoundMessage = `Paper beats rock. You win! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        postRoundMessage = `Paper beats rock. You win! Your score is ${humanScore} and the computer's score is ${computerScore}.`;
         console.log(postRoundMessage);
     }
     if(humanChoice == "paper" && computerChoice == "scissors") {
         computerScore++;
-        postRoundMessage = `Scissors beats paper. You lose! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        postRoundMessage = `Scissors beats paper. You lose! Your score is ${humanScore} and the computer's score is ${computerScore}.`;
         console.log(postRoundMessage);
     }
     if(humanChoice == "rock" && computerChoice == "scissors") {
         humanScore++;
-        postRoundMessage = `Rock beats scissors. You win! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        postRoundMessage = `Rock beats scissors. You win! Your score is ${humanScore} and the computer's score is ${computerScore}.`;
         console.log(postRoundMessage);
     }
     if(humanChoice == "rock" && computerChoice == "paper") {
         computerScore++;
-        postRoundMessage = `Paper beats rock. You lose! Your score is ${humanScore} and the computer's score is ${computerScore}`;
+        postRoundMessage = `Paper beats rock. You lose! Your score is ${humanScore} and the computer's score is ${computerScore}.`;
         console.log(postRoundMessage);
+    }
+
+    textDisplay.textContent = postRoundMessage;
+    resultDisplay.textContent = "Player: " + humanScore + " | Computer: " + computerScore;
+    if (humanScore === 5 || computerScore === 5) {
+        const winner = humanScore === 5 ? "You" : "The computer";
+        textDisplay.textContent += `\n${winner} won the game!`;
     }
 }
 
@@ -101,5 +130,6 @@ if(computerScore > humanScore) {
 } else {
     finalWinner = "nobody.";
     console.log(`Well played! Your final score is ${humanScore} and the computer's final score is ${computerScore}. The winner is ${finalWinner}. It's a tie!`);
+}
 }
 }
